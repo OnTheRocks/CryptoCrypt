e7bd14f249c2d39f9c042b960e82a3c91211054e
OnTheRocks <nathan_huber@yahoo.com>
2022-01-30 23:15:06 -0600

    Update server.js to Serve up statis asset

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const app = express()
 
 app.use(cors())
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.get('/', (req, res) => {
   res.json("HOWDY!")
 })
